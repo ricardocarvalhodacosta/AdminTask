699a4af344fbd2527729af07908555b7defd275d
Ricardo Carvalho da Costa <115588683+ricardocarvalhodacosta@users.noreply.github.com>
2023-05-04 18:50:05 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TaskManager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Task> tasks = new List<Task>();
+        }
+    }
+}
---
+++ Task.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TaskManager
+{
+    public class Task
+    {
+        public string Projeto { get; set; }
+        public string Descricao { get; set; }
+        public DateTime Data1 { get; set; }
+        public DateTime Data2 { get; set; }
+        public bool Status { get; set; }
+    }
+}
